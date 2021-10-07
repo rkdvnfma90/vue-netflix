47261e25dd4c7785cd50d656a600bd2419d2a496
aB <rkdvnfma90@hanmail.net>
2021-10-07 09:54:49 +0900

    refactor: 오타수정

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from '@/App';
 import Modal from '@/components/Modal';
 import LoadingSpinner from '@/components/LoadingSpinner';
 import Toast from '@/components/Toast';
-import ScrolltoTop from '@/components/ScrolltoTop';
+import ScrollToTop from '@/components/ScrollToTop';
 import store from '@/store';
 import router from '@/routes';
 
@@ -12,7 +12,7 @@ const app = createApp(App);
 app.component('Modal', Modal);
 app.component('LoadingSpinner', LoadingSpinner);
 app.component('Toast', Toast);
-app.component('ScrolltoTop', ScrolltoTop);
+app.component('ScrolltoTop', ScrollToTop);
 app.use(store);
 app.use(router);
 app.mount('#app');
